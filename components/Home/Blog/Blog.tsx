3d970332d877c9cddd05258374e578fbc2fc70c1
Paul <paulhyunchong@gmail.com>
2021-03-03 19:54:19 -0800

    Makes blog page and blog section use same styles

--- components/Home/Blog/Blog.tsx
+++ components/Home/Blog/Blog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from './blog.module.scss';
+import styles from '../../../styles/blog-page.module.scss';
 
 export default function Blog({ posts }) {
   return (
@@ -13,14 +13,37 @@ export default function Blog({ posts }) {
           {posts.map((post) => {
             return (
               <li key={post.path}>
-                <article>
+                <article
+                  key={post.title}
+                  className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
+                >
                   <Link href={post.path}>
-                    <a className={`space-y-4 ${styles.blogpost}`}>
-                      <h2 className="text-xl font-bold">{post.title}</h2>
-                      <p className="text-lg">{post.snippet}</p>
-                      <p className="text-base font-semibold">Read more</p>
+                    <a className="text-lg md:text-xl lg:text-2xl font-bold mb-4 tracking-tighter transition-colors duration-500">
+                      {post.title}
                     </a>
                   </Link>
+
+                  <p className="mb-4 tracking-wide text-sm md:text-base">
+                    {post.snippet}
+                  </p>
+
+                  <p className="text-xs md:text-sm font-semibold tracking-tighte">
+                    Read more{' '}
+                    <svg
+                      className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="#111827"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="3"
+                        d="M13 5l7 7-7 7M5 5l7 7-7 7"
+                      />
+                    </svg>
+                  </p>
                 </article>
               </li>
             );
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -13,7 +13,7 @@ export default function BlogPage({ directories, mapOfFiles }) {
         className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
       >
         <Link href={`/blog/${directory}/[slug]`} as={post.path}>
-          <a className="text-lg md:text-xl lg:text-2xl font-bold underline mb-4 tracking-tighter transition-colors duration-500">
+          <a className="text-lg md:text-xl lg:text-2xl font-bold mb-4 tracking-tighter transition-colors duration-500">
             {post.title}
           </a>
         </Link>
@@ -22,8 +22,22 @@ export default function BlogPage({ directories, mapOfFiles }) {
           {post.snippet}
         </p>
 
-        <p className="text-xs md:text-sm font-semibold tracking-tighter">
-          Read more
+        <p className="text-xs md:text-sm font-semibold tracking-tighte">
+          Read more{' '}
+          <svg
+            className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#111827"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="3"
+              d="M13 5l7 7-7 7M5 5l7 7-7 7"
+            />
+          </svg>
         </p>
       </article>
     ));
