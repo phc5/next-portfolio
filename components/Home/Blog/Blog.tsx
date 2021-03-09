94ae29ea04737a1002781e8210955caf62c7d37c
Paul <paulhyunchong@gmail.com>
2021-03-08 19:09:12 -0800

    Makes whole blog post clickable

--- components/Home/Blog/Blog.tsx
+++ components/Home/Blog/Blog.tsx
@@ -13,38 +13,38 @@ export default function Blog({ posts }) {
           {posts.map((post) => {
             return (
               <li key={post.path}>
-                <article
-                  key={post.title}
-                  className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
-                >
-                  <Link href={post.path}>
+                <Link href={post.path}>
+                  <article
+                    key={post.title}
+                    className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
+                  >
                     <a className="text-lg md:text-xl lg:text-2xl font-bold mb-4 tracking-tighter transition-colors duration-500">
                       {post.title}
                     </a>
-                  </Link>
 
-                  <p className="mb-4 tracking-wide text-sm md:text-base">
-                    {post.snippet}
-                  </p>
+                    <p className="mb-4 tracking-wide text-sm md:text-base">
+                      {post.snippet}
+                    </p>
 
-                  <p className="text-xs md:text-sm font-semibold tracking-tighte">
-                    Read more{' '}
-                    <svg
-                      className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="#111827"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="3"
-                        d="M13 5l7 7-7 7M5 5l7 7-7 7"
-                      />
-                    </svg>
-                  </p>
-                </article>
+                    <p className="text-xs md:text-sm font-semibold tracking-tighte">
+                      Read more{' '}
+                      <svg
+                        className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="#111827"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="3"
+                          d="M13 5l7 7-7 7M5 5l7 7-7 7"
+                        />
+                      </svg>
+                    </p>
+                  </article>
+                </Link>
               </li>
             );
           })}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -9,38 +9,38 @@ import styles from '../../styles/blog-page.module.scss';
 export default function BlogPage({ directories, mapOfFiles }) {
   const allPosts = directories.map((directory) => {
     const posts = mapOfFiles[directory].map((post) => (
-      <article
-        key={post.title}
-        className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
-      >
-        <Link href={`/blog/${directory}/[slug]`} as={post.path}>
+      <Link href={`/blog/${directory}/[slug]`} as={post.path}>
+        <article
+          key={post.title}
+          className={`px-8 py-6 lg:col-span-1 flex flex-col rounded-sm shadow-lg hover:shadow-2xl cursor-pointer transition-shadow duration-500 blog-page-item ${styles['blog-page-item']}`}
+        >
           <a className="text-lg md:text-xl lg:text-2xl font-bold mb-4 tracking-tighter transition-colors duration-500">
             {post.title}
           </a>
-        </Link>
 
-        <p className="mb-4 tracking-wide text-sm md:text-base">
-          {post.snippet}
-        </p>
+          <p className="mb-4 tracking-wide text-sm md:text-base">
+            {post.snippet}
+          </p>
 
-        <p className="text-xs md:text-sm font-semibold tracking-tighte">
-          Read more{' '}
-          <svg
-            className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="#111827"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="3"
-              d="M13 5l7 7-7 7M5 5l7 7-7 7"
-            />
-          </svg>
-        </p>
-      </article>
+          <p className="text-xs md:text-sm font-semibold tracking-tighte">
+            Read more{' '}
+            <svg
+              className="inline-block opacity-0 h-3 w-3 transition-opacity duration-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#111827"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              />
+            </svg>
+          </p>
+        </article>
+      </Link>
     ));
     return posts.length > 0 ? (
       <div key={directory} className="mt-16">
